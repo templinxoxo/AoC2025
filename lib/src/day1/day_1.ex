74fa0bc7740d701c08e940f852d2990bab79cdc0
Michal Templin <michaltemplin@surferseo.com>
2025-12-01 06:30:34 +0100

    feat: day1, pt1

--- lib/src/day1/day_1.ex
+++ lib/src/day1/day_1.ex
@@ -3,11 +3,14 @@ defmodule Aoc.Day1 do
   Day 1 solutions
   https://adventofcode.com/2025/day/1
   """
+
+  @starting_position 50
   def execute_part_1(data \\ fetch_data()) do
     data
     |> parse_input()
-
-    0
+    |> get_safe_combination([@starting_position])
+    |> Enum.filter(fn value -> value == 0 end)
+    |> length()
   end
 
   def execute_part_2(data \\ fetch_data()) do
@@ -16,18 +19,32 @@ defmodule Aoc.Day1 do
 
     0
   end
+  def get_safe_combination([], combination) do
+    Enum.reverse(combination)
+  end
+
+  def get_safe_combination([move | moves], [position  | _] = combination) do
+    new_position = case move do
+      { :right, value } -> rem(position + value,  100)
+      { :left, value } -> rem(position - value + 100, 100)
+    end
+
+    get_safe_combination(moves, [new_position] ++ combination)
+  end
 
   # helpers
   def fetch_data() do
-    day = "1"
-    Aoc.Utils.Api.get_input(day)
+    Aoc.Utils.Api.get_input("1")
   end
 
   def parse_input(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(fn row ->
-      row
+    |> Enum.map(fn "R"<>value ->
+      { :right, String.to_integer(value) }
+    "L"<>value ->
+      { :left, String.to_integer(value) }
     end)
+    |> IO.inspect()
   end
 end
--- lib/src/day1/day_1.test.exs
+++ lib/src/day1/day_1.test.exs
@@ -3,15 +3,24 @@ defmodule Aoc.Day1Test do
 
   alias Aoc.Day1, as: Subject
 
-  # replace with test data from the AoC puzzle page
   @test_data """
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
 
   """
 
   test "execute_part_1/1" do
     result = Subject.execute_part_1(@test_data)
 
-    assert result == 0
+    assert result == 3
   end
 
   test "execute_part_2/1" do
