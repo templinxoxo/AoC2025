6cf1adfd5749f9df795eabfaec80ad27ba1e8f4d
Michal Templin <michaltemplin@surferseo.com>
2025-12-03 07:36:36 +0100

    feat: day3, pt1&2

--- lib/src/day3/day_3.ex
+++ lib/src/day3/day_3.ex
@@ -6,16 +6,32 @@ defmodule Aoc.Day3 do
   def execute_part_1(data \\ fetch_data()) do
     data
     |> parse_input()
-
-    0
+    |> Enum.map(&(find_max_joltage(&1, 2) |> to_int()))
+    |> Enum.sum()
   end
 
   def execute_part_2(data \\ fetch_data()) do
     data
     |> parse_input()
-
-    0
+    |> Enum.map(&(find_max_joltage(&1, 12) |> to_int()))
+    |> Enum.sum()
   end
+
+  def find_max_joltage(_numbers, 0), do: []
+
+  def find_max_joltage(numbers, index_from_back) do
+    max_number =
+      numbers
+      |> Enum.slice(0..(0 - index_from_back)//1)
+      |> Enum.max()
+
+    index = Enum.find_index(numbers, &(&1 == max_number))
+    remaining_numbers = Enum.slice(numbers, (index + 1)..-1//1)
+
+    [max_number] ++ find_max_joltage(remaining_numbers, index_from_back - 1)
+  end
+
+  def to_int(list), do: list |> Enum.join("") |> String.to_integer()
 
   # helpers
   def fetch_data() do
@@ -27,7 +43,8 @@ defmodule Aoc.Day3 do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(fn row ->
-      row
+      String.split(row, "", trim: true)
+      |> Enum.map(&String.to_integer/1)
     end)
   end
 end
